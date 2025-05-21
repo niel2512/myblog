660e64fcf2c928c931e38d53647516cd1070cb2d
niel2512 <nielyusuf17@gmail.com>
2025-05-21 19:18:24 +0700

    Grouping routes Post

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,15 @@ Route::get('/contact', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
 // route ini harus ditulis sebelum route model binding
-Route::get('/dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::post('/dashboard', [PostDashboardController::class, 'store'])->middleware(['auth', 'verified']);
-Route::get('dashboard/create', [PostDashboardController::class, 'create'])->middleware(['auth', 'verified']);
-
+Route::get('/dashboard', [PostDashboardController::class, 'index'])->name('dashboard');
+Route::post('/dashboard', [PostDashboardController::class, 'store']);
+Route::get('dashboard/create', [PostDashboardController::class, 'create']);
 // ini adalah route model binding
-Route::get('dashboard/{post:slug}', [PostDashboardController::class, 'show'])->middleware(['auth', 'verified']);
+Route::delete('/dashboard/{post:slug}', [PostDashboardController::class, 'destroy']);
+Route::get('dashboard/{post:slug}', [PostDashboardController::class, 'show']);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
